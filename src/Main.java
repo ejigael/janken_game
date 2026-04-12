db70cba1425464724315bdf9348689f87458f5e1
ejigael <n0123784569@gmail.com>
2026-04-13 08:05:20 +0900

    判定条件手前

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 class Main {
     public static void main(String[] args) {
         Main m = new Main();
@@ -12,8 +13,8 @@ class Main {
         System.out.println("入力された文字は「" + num + "」です");
         return num;
         /*実行コマンド
-        javac -encoding UTF-8 Main.java
-        java -Dfile.encoding=UTF-8 Main*/
+        javac -encoding UTF-8 src/Main.java
+        java -Dfile.encoding=UTF-8 src/Main.java*/
     }
 
     public void comparison(int num) {
@@ -22,14 +23,17 @@ class Main {
         while(a) {
             if(num > 2) {
                 System.out.println("もう一度入力してください");
-                System.out.println("キーボード入力してください");
-                Scanner scan = new Scanner(System.in);
-                num = scan.nextInt();
-                System.out.println("入力された文字は「" + num + "」です");
+                num = Setting();
             } else {
                 a = false;
             }
         }
         System.out.println("finish");//確認用
+        Random random = new Random();
+        int number = random.nextInt(3);
+        System.out.println(number);
+        if (array[number] == array[num]) {
+            System.out.println("あいこ");
+        } else if()
     }
 }
